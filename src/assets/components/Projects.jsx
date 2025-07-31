7f73a81e40ecae3646d70a7111f6ec6a538fc6fb
Ritesh Kumar <riteshandshan@gmail.com>
2025-07-31 13:00:42 +0530

    thirdcommit

--- src/assets/components/Projects.jsx
+++ src/assets/components/Projects.jsx
@@ -36,7 +36,7 @@ function Projects() {
       logo: "/react.png",
       name: "ReactJS",
       desc: "A modern React portfolio website.",
-      link: "https://github.com/yourusername/react-portfolio"
+      link: "https://github.com/riteshkumar34/Portfolio.git"
     },
     {
       id: 7,
